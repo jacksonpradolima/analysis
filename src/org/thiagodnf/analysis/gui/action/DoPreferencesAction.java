6a2fdb4f4f6c855dafdb9811ef7638797698e384
Thiago Nascimento <thiagodnf@gmail.com>
2015-11-01 20:36:11 -0200

    refactoring

--- src/org/thiagodnf/analysis/gui/action/DoPreferencesAction.java
+++ src/org/thiagodnf/analysis/gui/action/DoPreferencesAction.java
@@ -2,49 +2,40 @@ package org.thiagodnf.analysis.gui.action;
 
 import java.awt.event.ActionEvent;
 
-import javax.swing.AbstractAction;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
 import org.thiagodnf.analysis.gui.window.MainWindow;
-import org.thiagodnf.analysis.gui.window.MessageBox;
 import org.thiagodnf.analysis.gui.window.PreferencesWindow;
 import org.thiagodnf.analysis.util.SettingsUtils;
 
-public class DoPreferencesAction extends AbstractAction {
+public class DoPreferencesAction extends DoAction {
 
 	private static final long serialVersionUID = -2332276187918581439L;
 	
-	protected JFrame parent;
-	
-	public DoPreferencesAction(JFrame parent){
-		this.parent = parent;
+	public DoPreferencesAction(JFrame parent) {
+		super(parent);
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void execute(ActionEvent event) throws Exception {
 		PreferencesWindow preferencesWindow = new PreferencesWindow(parent);
 		
 		if (preferencesWindow.showOptionDialog() == JOptionPane.YES_OPTION) {
-			// Save the user preferences on pc 
+			// Save the user preferences on pc
 			preferencesWindow.save();
-			
-			try {
-				if (userChangedTheLookAndFeel()) {
-					UIManager.setLookAndFeel(SettingsUtils.getLookAndFeel());
 
-					// Update look and feel
-					SwingUtilities.updateComponentTreeUI(parent);
-					parent.pack();
-				}
-			
-				((MainWindow) parent).getResultTable().reload();
-			} catch (Exception ex) {
-				MessageBox.error(parent, ex.getMessage());
-				ex.printStackTrace();
+			if (userChangedTheLookAndFeel()) {
+				UIManager.setLookAndFeel(SettingsUtils.getLookAndFeel());
+
+				// Update look and feel
+				SwingUtilities.updateComponentTreeUI(parent);
+				parent.pack();
 			}
+
+			((MainWindow) parent).getResultTable().reload();
 		}
 	}
 	
